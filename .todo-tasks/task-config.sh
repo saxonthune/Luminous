d63292f783274a66ddcb5fca0ca73ccfd6d7af84
saxonthune <st@saxon.zone>
2026-07-14 19:28:48 -0400

    chore: double todo-task turn and budget caps

--- .todo-tasks/task-config.sh
+++ .todo-tasks/task-config.sh
@@ -18,8 +18,8 @@ BUILD_CMD="just build"
 TEST_CMD="just test"
 
 # Budget caps for headless Claude sessions (USD)
-MAX_BUDGET="5.00"
-RETRY_BUDGET="3.00"
+MAX_BUDGET="10.00"
+RETRY_BUDGET="6.00"
 
 # Maximum retry attempts when build/test fails
 MAX_RETRIES=4
